80ae2c0a352cefba8cd78c143f7fd0884ec5b093
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-11-01 12:33:50 -0400

    Python is long but works

--- assignment.py
+++ assignment.py
@@ -1,19 +1,26 @@
+#this program is very innefficant and i know that but by the time i was done i needed to start on php/html 
+# Sorry it's such a long program that does not need to be long
+# I spent too much time perfecting this that i ran out of time to make a simple version 
 import math
 lGtopping1tax = (1.00 + 6.00 )
 lg1tax = (0.91)
-LGtopping2tax = (1.75 + 6.00 + 1.01)
+LGtopping2tax = (1.75 + 6.00)
 lg2tax = (1.01)
-LGtopping3tax = (2.50 + 6.00 + 1.11)
+LGtopping3tax = (2.50 + 6.00 )
 lg3tax = (1.11)
-LGtopping4tax = (3.35 + 6.00 + 1.22)
+LGtopping4tax = (3.35 + 6.00)
 lg4tax = (1.22)
 
-XLtopping1tax = (11.00 + 1.43)
-XLtopping2tax = (11.75 + 1.53)
-XLtopping3tax = (12.50 + 1.63)
-XLtopping4tax = (13.35 + 1.74)
-print("\nWelcome to pythonHut\n")
-pizza_yn = str(input("Do you want a pizza: ")) 
+XLtopping1tax = (11.00)
+xltax1 = (1.43)
+XLtopping2tax = (11.75 )
+xltax2 = (1.53)
+XLtopping3tax = (12.50)
+xltax3 = (1.63)
+XLtopping4tax = (13.35)
+xltax4 = (1.74)
+print("\nWelcome to pythonHut! \n")
+pizza_yn = str(input("Do you want to order a pizza: ")) 
 while True:
     if pizza_yn.lower() == "yes":
         print("\nGreat let's get started! ")
@@ -32,21 +39,28 @@ while True:
          else: 
                    print("Hope you enjoy your pizza! ")
     elif size.lower() ==  'large' and num_toppings == '2':
-             print(f"\nYou chose a {size} pizza with {num_toppings} toppings and your total is ${LGtopping2tax:.2f}")
-             again = input("\nWould you like to order another pizza? ")
-             if again == 'yes':
+         print(f"\nSubtotal: ${LGtopping2tax:.2f} ")
+         print(f"Tax: ${lg2tax:.2f}")
+         print(f"Total: ${lg2tax + LGtopping2tax:.2f}")
+
+         again = input("\nWould you like to order another pizza? ")
+         if again == 'yes':
                 continue
-             else: 
+         else: 
                    print("Hope you enjoy your pizza! ")
     elif size.lower() == 'large' and num_toppings == '3':
-             print(f"\nYou chose a {size} pizza with {num_toppings} toppings and your total is ${LGtopping3tax:.2f}")
+             print(f"\nSubtotal: ${LGtopping3tax:.2f} ")
+             print(f"Tax: ${lg3tax:.2f}")
+             print(f"Total: ${lg3tax + LGtopping3tax:.2f}")
              again = input("\nWould you like to order another pizza? ")
              if again == 'yes':
                 continue
              else: 
                    print("Hope you enjoy your pizza! ")
     elif size.lower() ==  'large' and num_toppings == '4':
-            print(f"\nYou chose a {size} pizza with {num_toppings} toppings and your total is ${LGtopping4tax:.2F}")
+            print(f"\nSubtotal: ${LGtopping4tax:.2f} ")
+            print(f"Tax: ${lg4tax:.2f}")
+            print(f"Total: ${lg4tax + LGtopping4tax:.2f}")
             again = input("\nWould you like to order another pizza? ")
             if again == 'yes':
                 continue
@@ -56,33 +70,41 @@ while True:
 
 #XL 
     elif size.upper() == 'XL' and num_toppings == '1':
-             print(f"\nYou chose a {size} pizza with {num_toppings} topping and your total is ${XLtopping1tax:.2f}")
-             again = input("\nWould you like to order another pizza? ")
-             if again == 'yes':
-                continue
-             else: 
-                   print("Hope you enjoy your pizza! ")
-    elif size.upper() == 'XL' and num_toppings == '2':
-             print(f"\nYou chose a {size} pizza with {num_toppings} toppings and your total is ${XLtopping2tax:.2f}")
-             again = input("\nWould you like to order another pizza? ")
-             if again == 'yes':
-                continue
-             else: 
-                   print("Hope you enjoy your pizza!")
-    elif size.upper() == 'XL' and num_toppings == '3':
-            print(f"\nYou chose a {size} pizza with {num_toppings} toppings and your total is ${XLtopping3tax:.2f}")
+            print(f"\nSubtotal: ${XLtopping1tax:.2f} ")
+            print(f"Tax: ${xltax1:.2f}")
+            print(f"Total: ${xltax1 + XLtopping1tax:.2f}")
             again = input("\nWould you like to order another pizza? ")
             if again == 'yes':
                 continue
             else: 
                    print("Hope you enjoy your pizza! ")
-    elif size.upper() == 'XL' and num_toppings == '4':
-            print(f"\nYou chose a {size} pizza with {num_toppings} toppings and your total is ${XLtopping4tax:.2f}")
+            
+    elif size.upper() == 'XL' and num_toppings == '2':
+            print(f"\nSubtotal: ${XLtopping2tax:.2f} ")
+            print(f"Tax: ${xltax2:.2f}")
+            print(f"Total: ${xltax2 + XLtopping2tax:.2f}")
             again = input("\nWould you like to order another pizza? ")
-
-    if again == 'yes':
-          continue
-   
-    
-    else:
-        break
+            if again == 'yes':
+                continue
+            else:
+                   print("Hope you enjoy your pizza! ")
+    elif size.upper() == 'XL' and num_toppings == '3':
+            print(f"\nSubtotal: ${XLtopping3tax:.2f} ")
+            print(f"Tax: ${xltax3:.2f}")
+            print(f"Total: ${xltax3 + XLtopping3tax:.2f}")
+            again = input("\nWould you like to order another pizza? ")
+            if again == 'yes':
+                continue
+            else:
+                   print("Hope you enjoy your pizza! ")
+            
+    elif size.upper() == 'XL' and num_toppings == '4':
+            print(f"\nSubtotal: ${XLtopping4tax:.2f} ")
+            print(f"Tax: ${xltax4:.2f}")
+            print(f"Total: ${xltax4 + XLtopping4tax:.2f}")
+            again = input("\nWould you like to order another pizza? ")
+            if again == 'yes':
+                continue
+            else:
+                print("Hope you enjoy your pizza! ")
+                break
